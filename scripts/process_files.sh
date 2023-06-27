f86e87ac55ccbdf1cc2b646b04212ff6acd63e57
Tejaswini Koduri <koduri.tejaswini58@gmail.com>
2023-06-27 12:55:48 -0400

    github actions for files as input

---
+++ scripts/process_files.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+names_file="$1"
+location_file="$2"
+
+# Process the files as needed
+echo "Processing names file: $names_file"
+echo "Processing location file: $location_file"
+
+# Add your logic here to work with the files
+
